bb6a3498e3b0c54bb6d48993607343768e4382b6
ML-std <ML-std@github. com>
2020-07-11 16:48:06 +0300

    Logo has added, some unnecessary files has been removed

--- src/main/java/GUI/Launcher.java
+++ src/main/java/GUI/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -20,6 +21,8 @@ public class Launcher extends Application {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("loginController.fxml")));
             stage.setScene(new Scene(root));
             stage.setTitle("Gym Database Management System");
+            Image icon = new Image("gym.jpg");
+            stage.getIcons().add(icon);
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
